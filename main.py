0dc61778940fe4d873f675eb19b0eaec4694c552
Alejacc123 <alejandra.amayap@upb.edu.co>
2025-10-06 13:27:11 -0500

    Se agraga menu

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-def suma (a, b):
-    return a + b
+def suma(a, b):
+    total = a + b
+    return total
 def resta(a, b):
-    return a - b
+    total = a - b
+    return total
 def multiplicacion (a, b):
-    return a * b
+    total = a * b
+    return total
 def division(a, b):
-    if b > 0 :
-        return a / b
-    
+    if b != 0:
+        total = a / b
+        return total
+def exponencial(a,b):
+    total = a ** b
+    return total
+
+def modulo(a,b):
+    total = a * b
+    return a % b
+        
+print("--------Bienvenido---------")
+f=True
+
+while(f):
+    print("1. Suma / 2. Resta / 3. Multiplicacion / 4. Division / 5. Exponencial / 6. Modulo / 7.Fin")
+
+    a = int(input("Ingresa el primer numero: "))
+    b = int(input("Ingresa el segundo numero: "))
+
+    op=int(input("¿Que operacion usaras?: "))
+    if op==7:
+        f=False
+        break
+    if op==1:
+        print("Resultado suma:  ", suma(a,b))
+    elif op==2:
+        print("Resultado resta:  ", resta(a,b))
+    elif op==3:
+        print("Resultado multipliacion:  ", multiplicacion(a,b))
+    elif op==4:
+        print("Resultado division:  ", division(a,b))
+    elif op==5:
+        print("Resultado Exponencial", exponencial(a,b))
+    elif op==6:
+        print("Resultado Modulo", modulo(a,b))
+    else:
+        print("Opcion inexistente")
